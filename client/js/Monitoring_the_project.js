e6a118f482037e4eea10d138824e0fc37f882b92
batzio <aniandata1234@gmail.com>
2023-03-28 11:42:27 +0300

    first commit

---
+++ client/js/Monitoring_the_project.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var password = localStorage.getItem('password')
+//  להביא את הציון המשוקלל עבור כל תלמיד ולשים בעמודה של הציונים
+    // Get the table element by its ID
+    // const table = document.getElementById("myTable");
+
+    // // Create the column title element and set its text content
+    // const columnTitle = document.createElement("th");
+    // columnTitle.textContent = "Column Title";
+
+    // // Create the row title element and set its text content
+    // const rowTitle = document.createElement("th");
+    // rowTitle.textContent = "Row Title";
+
+    // // Insert the column title element into the first row
+    // const firstRow = table.getElementsByTagName("tr")[0];
+    // firstRow.insertBefore(columnTitle, firstRow.firstChild);
+
+    // // Insert the row title element into the first column of each row
+    // const rows = table.getElementsByTagName("tr");
+    // for (let i = 1; i < rows.length; i++) {
+    //     const cell = document.createElement("td");
+    //     cell.textContent = "Cell Value";
+    //     rows[i].insertBefore(rowTitle.cloneNode(true), rows[i].firstChild);
+    //     rows[i].appendChild(cell);
+    // }
+});
