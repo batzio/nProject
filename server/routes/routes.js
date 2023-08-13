5799c2c5c432c205d8604a14ddd9bf7655e2555e
batzio <aniandata1234@gmail.com>
2023-08-13 18:02:29 +0300

    Changing route names

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -10,12 +10,17 @@ router.post('/addstudent', projects_routes.createStudent);
 router.get('/projects', projects_routes.getProjects);
 router.get('/explanation/:id', projects_routes.getExplanation);
 router.get('/status/:id', projects_routes.getStatus);
-router.get('/moderator/:password', projects_routes.getPasswordMod);
-router.get('/student/:password', projects_routes.getPasswordSdt);
+router.get('/getModeratorPwd/:password', projects_routes.getPasswordMod);
+router.get('/student/:id', projects_routes.getIdSdt);
+router.get('/moderator/:id', projects_routes.getIdMod);
+router.get('/getStudentPwd/:password', projects_routes.getPasswordSdt); 
 router.get('/students', projects_routes.getStudents);
+router.get('/moderators', projects_routes.getModerators);
 router.get('/project/:id', projects_routes.getProject);
+// router.get('/getstudent:username', projects_routes.getUsername);
 
 router.put('/updatProject/:id', projects_routes.updateProject);
-
+router.put('/updateStudent/:id', projects_routes.updateStudent);
+router.put('/updateModerator/:id', projects_routes.updateModerator);
 
 module.exports = router;
