90c3af908c358e73a9e2b629a37e27d6f4b6d8f9
batzio <aniandata1234@gmail.com>
2023-11-13 12:25:48 +0200

    Arrangement of functions

--- client/js/home_page.js
+++ client/js/home_page.js
@@ -1,11 +1,10 @@
-jQuery(function($) {
-    // Your code here
-//   });
-// $(document).ready(function () {
-    var username = localStorage.getItem('username')
-    document.getElementById("title").innerHTML = "שלום, " + username;
-    // });
-    // function check(){
+// const { use } = require("../../server/routes/routes");
+
+jQuery(function ($) {
+    var name = localStorage.getItem('name')
+    // console.log('name - ', name)
+    // alert('name')
+    document.getElementById("title").innerHTML = "שלום, " + name;
 
     var project = document.createElement("option")
 
@@ -19,121 +18,27 @@ jQuery(function($) {
     if (data === "student") {
         btnEditProj.style.visibility = "hidden";
         projects_name.appendChild(project)
-
-        $.ajax({
-            type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
-            url: '/projects',
-            success: function (result) {
-                //adding all the options
-                projects_name = document.getElementById("projects_name")
-                index = 0;
-                $.each(result, function (index, value) {
-                    var project = document.createElement("option")
-                    project.setAttribute('value', index)
-                    project.setAttribute("id", value._id)
-                    project.setAttribute("project_name", value.name)
-                    project.setAttribute("project_subject", value.subject)
-                    project.setAttribute("details", value.details)
-                    project.setAttribute("project_type", value.project_type)
-                    project.setAttribute("status", value.status)
-                    project.setAttribute("offer", value.offer)
-                    project.setAttribute("add_time", value.add_time)
-
-                    project.innerHTML = value.name
-                    projects_name.appendChild(project)
-                    index++;
-                    console.log('hggg')
-                });
-            },
-            error: function (jqXhr, textStatus, errorThrown) {
-                console.log(errorThrown);
-            }
-        });
-        // projects_name.addEventListener("change", function () {     //choose the id of project that selected
-        //     var select = document.getElementById('projects_name');
-        //     for (var i = 0; i < select.options.length; i++) {
-        //         var option = select.options[i];
-        //         if (option.selected) {
-        //             $.ajax({
-        //                 type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
-        //                 url: '/explanation/' + option.id,
-        //                 success: function (result) {
-        //                     // console.log(result[2].details)
-        //                     document.getElementById('id_explanation').value = result[0].details;
-
-        //                 },
-        //                 error: function (jqXhr, textStatus, errorThrown) {
-        //                     console.log(errorThrown);
-        //                 }
-        //             });
-
-        //             $.ajax({
-        //                 type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
-        //                 url: '/status/' + option.id,
-        //                 success: function (result) {
-        //                     // console.log(result)
-        //                     document.getElementById('status_id').value = result[0].status;
-
-        //                 },
-        //                 error: function (jqXhr, textStatus, errorThrown) {
-        //                     console.log(errorThrown);
-        //                 }
-        //             });
-        //             break;
-        //         }
-        //     }
-        // });
+        getAllProjectsDetails();
     }
     else {
         // document.getElementById("status_id").style.display = "none";
         // If the user is moderator he will see onle his projects
         //אם המשתמש הוא מנחה אז נביא את כל הפרויקטים שלו לפי האיי די שלו. ניתן יהיה לסדר את זה ברגע שהלוגין יהיה מוכן ונוכל להיכנס לאתר לפי שם משתמש וסיסמה
-        if (data === "moderator") {
+        if (data === "moderator" || data === "coordinator") {
+            var id = localStorage.getItem("modID");
             $.ajax({
                 type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
-                url: '/projects',
+                url: '/moderatorProjects/' + id,
                 success: function (result) {
-                    //adding all the options
-                    projects_name = document.getElementById("projects_name")
                     index = 0;
                     $.each(result, function (index, value) {
-                        var project = document.createElement("option")
-                        project.setAttribute('value', index)
-                        project.setAttribute("id", value._id)
-                        project.setAttribute("project_name", value.name)
-                        project.setAttribute("project_subject", value.subject)
-                        project.setAttribute("details", value.details)
-                        project.setAttribute("project_type", value.project_type)
-                        project.setAttribute("status", value.status)
-                        project.setAttribute("offer", value.offer)
-                        project.setAttribute("add_time", value.add_time)
-
-                        project.innerHTML = value.name
-                        projects_name.appendChild(project)
-                        index++;
-                        // console.log('hggg '+project.getAttribute('project_name'))
+                        getProjectsDetails(result[index], index);
                     });
-                    // // After the options have been added, check if "id_explanation" is not empty
-                    // if (document.getElementById("id_explanation").value !== "") {
-                    //     // Get the project ID from the selected option
-                    //     var selectedOption = projects_name.options[projects_name.selectedIndex];
-                    //     var projectId = selectedOption.getAttribute("id");
-
-                    //     console.log("Project ID:", projectId);
-                    //     btnEditProj.disable = false;
-                    // }
                 },
                 error: function (jqXhr, textStatus, errorThrown) {
                     console.log(errorThrown);
                 }
             });
-
-            // if (document.getElementById("id_explanation") != "") {
-            //     console.log("into if ", project.id)
-            //     btnEditProj.disable = false;
-
-            //     // document.getElementById('button').onclick = editProject(project.id);
-            // }
         }
 
     }
@@ -183,9 +88,10 @@ jQuery(function($) {
 });
 
 
-function editProject(id, isEdit) {
+function editProject(id) {
     // alert("in editProject - check edit btn - ", id)
     localStorage.setItem("isEdit", 'true');
+    // localStorage.setItem("isedit", 'true');
 
     $.ajax({
         type: 'GET', // define the type of HTTP verb we want to use (POST for our form)
@@ -196,20 +102,89 @@ function editProject(id, isEdit) {
             // console.log(data)
             // localStorage.setItem('update', 'true')
             localStorage.setItem('proj_id', data._id)
-            localStorage.setItem('name_proj', data.name)
-            localStorage.setItem('subject_proj', data.subject)
+            localStorage.setItem('english_name_proj', data.name_english)
+            localStorage.setItem('hebrew_name_proj', data.name_hebrew)
             localStorage.setItem('details_proj', data.details)
-            // var date = new Date(data.date);
-            // localStorage.setItem('date_keness', date.toISOString().slice(0, 10))
-            localStorage.setItem('status_proj', data.status)
             localStorage.setItem('type_proj', data.project_type)
+            localStorage.setItem('status_proj', data.status)
             localStorage.setItem('offer_proj', data.offer)
             localStorage.setItem('add_time_proj', data.add_time)
-
+            localStorage.setItem('single_or_couple_proj', data.single_or_couple)
+            localStorage.setItem('external_factor_proj', data.external_factor)
+            localStorage.setItem("external_party_email_proj", data.external_party_email)
             window.location.href = "/addproject";
         },
         error: function (err) {
             console.log("err" + err);
+        }
+    });
+}
+
+function getProjectsDetails(id, index) {
+    // alert('getProjectsDetails')
+    $.ajax({
+        type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
+        url: '/project/' + id,
+        success: function (result) {
+            var value = result[0];
+            var project = document.createElement("option")
+            project.setAttribute('value', index)
+            project.setAttribute("id", value._id)
+            project.setAttribute("name_english", value.name_english)
+            project.setAttribute("name_hebrew", value.name_hebrew)
+            project.setAttribute("details", value.details)
+            project.setAttribute("project_type", value.project_type)
+            project.setAttribute("status", value.status)
+            project.setAttribute("offer", value.offer)
+            project.setAttribute("add_time", value.add_time)
+            project.setAttribute("single_or_couple", value.single_or_couple)
+            project.setAttribute("external_factor", value.external_factor)
+            project.setAttribute("external_party_email", value.external_party_email)
+
+            console.log('name - ', value.name_hebrew);
+            // alert('name')
+            project.innerHTML = value.name_hebrew
+            projects_name.appendChild(project)
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+
+function getAllProjectsDetails(){
+    $.ajax({
+        type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
+        url: '/projects',
+        success: function (result) {
+            //adding all the options
+            // projects_name = document.getElementById("projects_name")
+            index = 0;
+            $.each(result, function (index, value) {
+                var project = document.createElement("option")
+                project.setAttribute('value', index)
+                project.setAttribute("id", value._id)
+                project.setAttribute("name_english", value.name_english)
+                project.setAttribute("name_hebrew", value.name_hebrew)
+                project.setAttribute("details", value.details)
+                project.setAttribute("project_type", value.project_type)
+                project.setAttribute("status", value.status)
+                project.setAttribute("offer", value.offer)
+                project.setAttribute("add_time", value.add_time)
+                project.setAttribute("single_or_couple", value.single_or_couple)
+                project.setAttribute("external_factor", value.external_factor)
+                project.setAttribute("external_party_email", value.external_party_email)
+
+
+                // console.log('name - ', value.name_hebrew);
+
+                project.innerHTML = value.name_hebrew;
+                projects_name.appendChild(project);
+                index++;
+            });
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
         }
     });
 }
