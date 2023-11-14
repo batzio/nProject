bc709137d1a81c4a5b001893ce3212bd9251bc56
batzio <aniandata1234@gmail.com>
2023-11-14 14:34:01 +0200

    Comments have been deleted

--- client/js/Login.js
+++ client/js/Login.js
@@ -8,21 +8,25 @@ function sub() {
   }
 
   else {
-    // alert('in else')
+    alert('in else')
     $.ajax({
       type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
       url: '/getStudentPwd/' + password,
       contentType: 'application/json',
       success: function (result) {
-        // console.log('JSON.stringify(result[0]) - '+JSON.stringify(result[0]))
-        // alert('in else check')
+        alert('in else check')
+        console.log('result[0] - '+result[0])
         if (JSON.stringify(result[0]) == undefined) {
-          alert('in undefined')
+        alert('in else check')
           login_mod(username, password);
-          // return;
         }
         else if (result[0].username == username && result[0].password == password) {
+          // console.log(result[0].username);
+          // console.log(result[0].password);
+          // alert("success to bring student")
           localStorage.setItem("data", "student")
+          var name = result[0].sdt_firstName + " " + result[0].sdt_lastName;
+          localStorage.setItem("name", name)
           location.href = "/assigAndsubDats"
         }
         else {
@@ -37,30 +41,15 @@ function sub() {
 }
 
 function login_mod(username, password) {
+  // console.log('username - ' + username + ' password - ' + password)
   alert('in log_mod')
   $.ajax({
     type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
     url: '/getModeratorPwd/' + password,
-    contentType: 'application/json',
     success: function (result) {
-      // console.log('in success in log_mod - ' + result[0])
-      // alert('in if')
       if (result[0].username == username && result[0].password == password) {
-        // console.log('username - ' + result[0].username + ' password - ' + result[0].password + ' id - ' + result[0].mod_ID)
-        // console.log('is_coor(result[0].mod_ID) - ' + is_coor(result[0].mod_ID))
-        // alert('in else - if its coor')
-        is_coor(result[0].mod_ID)
-        // console.log('is_coor(result[0].mod_ID) - ' + is_coor(result[0].mod_ID))
-        //   alert('is_coor(result[0].mod_ID)')
-        // if (is_coor(result[0].mod_ID)) {
-        //   // console.log('is_coor(result[0].mod_ID) - ' + is_coor(result[0].mod_ID))
-        //   // alert('is_coor(result[0].mod_ID)')
-        //   localStorage.setItem("data", "coordinator");
-        // }
-        // else {
-        //   localStorage.setItem("data", "moderator");
-        // }
-        // window.location.href = "/assigAndsubDats";
+        var name = result[0].mod_firstName + " " + result[0].mod_lastName;
+        is_coor(result[0].mod_ID, name, result[0]._id);
       }
       else {
         alert('שם משתמש ו/או הסיסמה שגויים')
@@ -73,20 +62,24 @@ function login_mod(username, password) {
 }
 
 
-function is_coor(id) {
-  console.log('id - ' + id)
-  alert('in is_coor')
+function is_coor(id, name, modID) {
+  // console.log('id - ' + id)
+  // alert('in is_coor')
   $.ajax({
     type: 'GET', // define the type of HTTP verb we want to use (GET for our form)
     url: '/getCoodinator',///' + id,
     contentType: 'application/json',
     success: function (result) {
+      // console.log('result[0] - ',result[0])
+      alert('result[0] - in is_coor')
       if (result[0].coo_ID == id) {
         localStorage.setItem("data", "coordinator");
       }
       else {
         localStorage.setItem("data", "moderator");
       }
+      localStorage.setItem("name", name)
+      localStorage.setItem("modID", modID)
       window.location.href = "/assigAndsubDats";
       // console.log('is_coor - ' + JSON.stringify(result[0]) )
       // alert('in success of is_coor')
@@ -103,7 +96,8 @@ function resetPassword() {
   alert('in reset')
   var username = document.getElementById("id_username").value;
   localStorage.setItem('username', username);
-  window.location.href = '/changepassword';
+  location.href = '/changepassword';
+  alert('in reset after all')
 }
 
 
