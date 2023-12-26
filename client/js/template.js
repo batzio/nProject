71368cb2d181651e0fbcea3b1713c5d0fabd4f7b
batzio <aniandata1234@gmail.com>
2023-12-26 11:03:25 +0200

    Deleting unimportant things, adding download func

--- client/js/template.js
+++ client/js/template.js
@@ -1,12 +1,71 @@
-function proposal_upload(){
+// function upload_files(fileId){
+//     var fileInput;
+//     var rptName;
+//     if(fileId === '0000'){
+//         fileInput = 'propFileInput';
+//         rptName = 'proposal_rpt';
+//     }
+//     else if( fileId === '0001'){
+//         fileInput = 'alfaFileInput';
+//         rptName = 'alfa_rpt'
+//     }
+//     else if(fileId === '0010'){
+//         fileInput = 'betaFileInput';
+//         rptName = 'beta_rpt'
+//     }
+//     else if(fileId === '0011'){
+//         fileInput = 'finalFileInput';
+//         rptName = 'final_rpt';
+//     }
+
+//     const formData = new FormData();  // Create a new FormData object
+//     const file = document.getElementById(fileInput).files[0];
+//     formData.append(rptName, file);
+
+//     $.ajax({
+//         url: '/uploadRepFiles/' + fileId + '/' + rptName,
+//         type: 'POST',
+//         data: formData,
+//         contentType: false,
+//         processData: false,
+//         success: function (data, textStatus, jQxhr) {
+//             // alert('in success in upload_files')
+//             location.reload();
+//         }
+//     });
+// }
+
+// jQuery(function ($) {
+//     con
+// })
+
+const id_sdt = localStorage.getItem('stdID')
+
+
+
+
+
+
+function proposal_upload(id){
+    // var b = localStorage.setItem('idFile', '0000');
+    // alert('id')
+    // console.log(id)
+    // alert('id')
+
+    if(id == ""){
+        id = id_sdt
+        // console.log(id)
+        // alert('id is empty')
+    }
     const formData = new FormData();  // Create a new FormData object
     // Assuming 'fileInput' is the id of file input element
     const file = document.getElementById('propFileInput').files[0];
     // Append the file to the formData
     formData.append('proposal_rpt', file);
+    // var id = document.get
 
     $.ajax({
-        url: '/uploadProposalRep',
+        url: '/uploadProposalRep/' + id,
         type: 'POST',
         data: formData,
         contentType: false,
@@ -18,7 +77,8 @@ function proposal_upload(){
     });
 }
 
-function alfa_upload() {
+function alfa_upload(id) {
+    // localStorage.setItem('idFile', '0001');
     const formData = new FormData();  // Create a new FormData object
     // Assuming 'fileInput' is the id of file input element
     const file = document.getElementById('alfaFileInput').files[0];
@@ -26,7 +86,7 @@ function alfa_upload() {
     formData.append('alfa_rpt', file);
 
     $.ajax({
-        url: '/uploadAlfaRep',
+        url: '/uploadAlfaRep/' + id,
         type: 'POST',
         data: formData,
         contentType: false,
@@ -38,15 +98,15 @@ function alfa_upload() {
     });
 }
 
-function beta_upload(){
+function beta_upload(id){
+    // localStorage.setItem('idFile', '0010');
     const formData = new FormData();  // Create a new FormData object
     // Assuming 'fileInput' is the id of file input element
     const file = document.getElementById('betaFileInput').files[0];
     // Append the file to the formData
     formData.append('beta_rpt', file);
-
     $.ajax({
-        url: '/uploadBetaRep',
+        url: '/uploadBetaRep/' + id,
         type: 'POST',
         data: formData,
         contentType: false,
@@ -58,7 +118,8 @@ function beta_upload(){
     });
 }
 
-function final_upload(){
+function final_upload(id){
+    // localStorage.setItem('idFile', '0011');
     const formData = new FormData();  // Create a new FormData object
     // Assuming 'fileInput' is the id of file input element
     const file = document.getElementById('finalFileInput').files[0];
@@ -66,7 +127,7 @@ function final_upload(){
     formData.append('final_rpt', file);
 
     $.ajax({
-        url: '/uploadFinalRep',
+        url: '/uploadFinalRep/' + id,
         type: 'POST',
         data: formData,
         contentType: false,
@@ -78,6 +139,19 @@ function final_upload(){
     });
 }
 
+
+function download_file(fileId) {
+    // var fileId = localStorage.getItem('idFile');
+    console.log('fileId - ', fileId)
+    alert('fileId')
+    $.ajax({
+      url: '/download/' + fileId, // Route on your server to handle download
+      method: 'GET',
+      success: function () {
+        console.log('File downloaded successfully!');
+      }
+    });
+  }
 
 //   function beforeppp() {
 //     // var id = localStorage.getItem('id')
