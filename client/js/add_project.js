2737deddad93795f947f24efbb479c1ec00afc7f
batzio <aniandata1234@gmail.com>
2023-12-26 11:08:29 +0200

    Deleting unimportant things and adding tests

--- client/js/add_project.js
+++ client/js/add_project.js
@@ -1,15 +1,14 @@
-// function per_add_proj() {
-// jQuery(function ($) {
 const isEdit = localStorage.getItem("isEdit")
 const modID = localStorage.getItem("modID")
-console.log('modID - ', modID)
-// alert('modID')
-// $(document).ready(function () {
+// console.log('modID - ', modID)
+
 jQuery(function ($) {
     var data = localStorage.getItem("data");
     localStorage.setItem("data", data);
 
-    
+    getFullDate();
+
+
     if (isEdit == 'true') {
         $('#title').html("עידכון פרויקט:")
         receiving_the_information();
@@ -19,14 +18,13 @@ jQuery(function ($) {
         var name = localStorage.getItem("name");
         document.getElementById('offer_id').value = name;
         document.getElementById('offer_id').innerHTML = name;
-        // alert('before getfulldate')
-        getFullDate_for_new();
+
+        var update_or_add = document.getElementById('id_group_update')      //לבדוק מחר אם זה נותן את התאריך של של היום הנוכחי או את התאריך של ההוספה - לא תקין
+        update_or_add.style.visibility = "hidden";
+
         localStorage.clear()
         $('#title').html("הוספת פרוייקט:")
-        // add_project();
     }
-    // }
-
 });
 
 function per_add_proj() {
@@ -40,41 +38,44 @@ function per_add_proj() {
     var outputSingleOrCouple = singleOrCoupleSelect.value;
     document.getElementById("input_single_or_couple_id").setAttribute('value', outputSingleOrCouple)
 
+    console.log('outputStatus - ', outputStatus)
+    console.log('outputSingleOrCouple - ', outputSingleOrCouple)
+    alert('outputStatus, outputSingleOrCouple')
+
+
     var email = document.getElementById('external_party_email_id').value;
     validateEmail(email)//check if email correct
 
-    var update_or_add = document.getElementById('id_group_update')      //לבדוק מחר אם זה נותן את התאריך של של היום הנוכחי או את התאריך של ההוספה - לא תקין
-    /* x.style.display = "none";*/
-    // var y = localStorage.getItem("yyyy")     //to check why its null
-    // console.log("edit - ", y)
-    // alert("edit - ")
     if (isEdit == 'true') {
-        update_or_add.style.display = 'block';          // Show
         var id = localStorage.getItem('proj_id')
         update_project(id);
     }
     else if (isEdit == 'false') {
-        update_or_add.style.display = 'none';           // Hide
         add_project(outputStatus, outputSingleOrCouple);
     }
 }
 
 
-function getFullDate_for_new() {
-    // alert('in getFullDate')
+function getFullDate() {
     const date = new Date();
     var month = date.getMonth() + 1;
     var year = date.getFullYear();
     var day = date.getDate();
 
     var fullDate = day + '/' + month + '/' + year;
-    // console.log('in getFullDate - ' + fullDate)
-    // alert('fullDate')
-    document.getElementById("add_time_id").value = fullDate;
-    document.getElementById('add_time_id').innerHTML = fullDate;
 
-    document.getElementById("update_time_id").value = fullDate;
-    document.getElementById('update_time_id').innerHTML = fullDate;
+    if (isEdit == 'true') {
+        document.getElementById("update_time_id").value = fullDate;
+        document.getElementById("update_time_id").innerHTML = fullDate;
+    }
+
+    if (isEdit == 'false') {
+        document.getElementById("add_time_id").value = fullDate;
+        document.getElementById('add_time_id').innerHTML = fullDate;
+    }
+
+
+
 }
 
 function receiving_the_information() {
@@ -88,30 +89,27 @@ function receiving_the_information() {
     var single_or_couple = localStorage.getItem('single_or_couple_proj')
     var external_factor = localStorage.getItem('external_factor_proj')
     var external_party_email = localStorage.getItem('external_party_email_proj')
-    var fullDate = getFullDate(date);
+
+
+    // console.log('status - ', status)
+    // console.log('single_or_couple - ', single_or_couple)
+    // alert('status, single_or_couple')
+
 
     document.getElementById("pjt_eng_id").setAttribute('value', english_name)
     document.getElementById("pjt_hbw_id").setAttribute('value', hebrew_name)
     document.getElementById("details_id").setAttribute('value', details)
     document.getElementById("project_type_id").setAttribute('value', project_type)
-    document.getElementById("status_id").setAttribute('value', status)
+    // document.getElementById("status_id").setAttribute('value', status)
+    document.getElementById('status_id').value = status;
     document.getElementById("offer_id").setAttribute('value', offer)
-    document.getElementById("add_time_id").setAttribute('value', fullDate)
-    document.getElementById("single_or_couple_id").setAttribute('value', single_or_couple)
+    document.getElementById("add_time_id").setAttribute('value', date)
+    // document.getElementById("single_or_couple_id").setAttribute('value', single_or_couple)
+    document.getElementById('single_or_couple_id').value = single_or_couple;
     document.getElementById("external_factor_id").setAttribute('value', external_factor)
     document.getElementById("external_party_email_id").setAttribute('value', external_party_email)
 }
 
-function getFullDate(fulldate) {
-    const date = new Date(fulldate);
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-    var day = date.getDate();
-
-    var fullDate = day + '/' + month + '/' + year;
-    return fullDate;
-    // document.getElementById("add_time_id").value = fullDate;
-}
 
 const validateEmail = (email) => {
     // alert('in validateEmail')
@@ -133,6 +131,7 @@ function update_project(id) {
             "details": $('#details_id').val(),
             "project_type": $('#project_type_id').val(),
             "status": $('#input_status_id').val(),
+            "update_time": $('#update_time_id').val(),
             "single_or_couple": $('#input_single_or_couple_id').val(),
             "external_factor": $('#external_factor_id').val(),
             "external_party_email": $('#external_party_email_id').val()
@@ -140,7 +139,17 @@ function update_project(id) {
         processData: false,
         encode: true,
         success: function (data, textStatus, jQxhr) {
-            location.href = "/home";
+            var status = document.getElementById('input_status_id').value;
+            var single_or_couple = document.getElementById('input_single_or_couple_id').value;
+            // console.log('status - ', status)
+            // alert('status')
+            if (status == 'open') {
+                location.href = "/home";
+            }
+            else {
+                add_project_id_to_students(id, single_or_couple);
+            }
+            // location.href = "/home";
         },
         error: function (jqXhr, textStatus, errorThrown) {
             console.log(errorThrown);
@@ -162,7 +171,6 @@ function add_project(outputStatus, outputSingleOrCouple) {
             "status": outputStatus,
             "offer": $('#offer_id').val(),
             "add_time": $('#add_time_id').val(),
-            "update_time": $('#update_time_id').val(),
             "single_or_couple": outputSingleOrCouple,
             "external_factor": $('#external_factor_id').val(),
             "external_party_email": $('#external_party_email_id').val(),
@@ -171,11 +179,7 @@ function add_project(outputStatus, outputSingleOrCouple) {
         processData: false,
         encode: true,
         success: function (data, textStatus, jQxhr) {
-            // console.log('data - ', data)
-            // alert('data._id')
-
             add_pro_to_mod(data._id);
-            // window.location.href = "/assigAndsubDats";
         },
         error: function (jqXhr, textStatus, errorThrown) {
             console.log(errorThrown);
@@ -196,7 +200,6 @@ function add_pro_to_mod(id_pjt) {
         encode: true,
         success: function (data, textStatus, jQxhr) {
             add_mod_to_pro(id_pjt);
-            // location.href = "/assigAndsubDats";
         },
         error: function (jqXhr, textStatus, errorThrown) {
             console.log(errorThrown);
@@ -205,9 +208,9 @@ function add_pro_to_mod(id_pjt) {
 }
 
 function add_mod_to_pro(id_pjt) {
-    console.log('add_mod_to_pro - ', id_pjt)
-    console.log(modID)
-    alert('add_mod_to_pro')
+    // console.log('add_mod_to_pro - ', id_pjt)
+    // console.log(modID)
+    // alert('add_mod_to_pro')
     $.ajax({
         type: 'PUT', // define the type of HTTP verb we want to use (GET for our form)
         url: '/updateIdModToProjet/' + id_pjt,
@@ -226,3 +229,43 @@ function add_mod_to_pro(id_pjt) {
         }
     });
 }
+
+function add_project_id_to_students(id_pjt, single_or_couple) {
+    // let text;
+    let id_sdt = prompt("הכנס תז של הסטודנט שעושה פרוייקט זה", "");
+    if (id_sdt == null || id_sdt == "") {
+        alert("חובה להכניס תז של הסטודנט");
+    } else {
+        update_student_id_pjt(id_sdt, id_pjt);
+    }
+    // document.getElementById("demo").innerHTML = text;
+    if (single_or_couple == 'couple') {
+        // let text1;
+        let id_second_sdt = prompt("הכנס תז של הסטודנט הנוסף שעושה פרוייקט זה", "");
+        if (id_second_sdt == null || id_second_sdt == "") {
+            alert("חובה להכניס תז של הסטודנט");
+        } else {
+        update_student_id_pjt(id_second_sdt, id_pjt);
+        }
+        // document.getElementById("demo").innerHTML = text1;
+    }
+    alert('הסתיים בהצלחה')
+}
+
+function update_student_id_pjt(id_sdt, id_pjt){
+    $.ajax({
+        type: 'PUT', // define the type of HTTP verb we want to use (GET for our form)
+        url: '/updateStudentIdPjt/' + id_sdt,
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "id_pjt": id_pjt
+        }),
+        success: function (result) {
+            window.location.href = '/home';
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+         
