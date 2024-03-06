1fd7c771a8dc95f16d72dd5a689c8b3435a41ec2
yael <yael25801@gmail.com>
2024-03-06 22:05:35 +0200

    loginCSS

--- client/js/Login.js
+++ client/js/Login.js
@@ -110,6 +110,36 @@ function resetPassword() {
   alert('in reset after all')
 }
 
+function showPassword(str) {
+  var x
+  switch(str){
+      case 'sdt':
+          x = document.getElementById("sdt_pswd_id");
+          break;
+      case 'mod':
+          x = document.getElementById('mod_pswd_id');
+          break;
+      case 'old':
+          x = document.getElementById('old_pwd_or_ID');
+          break;
+      case 'new':
+          x = document.getElementById('id_new_pwd');
+          break;
+      case 'again':
+          x = document.getElementById('id_again_new_pwd');
+          break;
+      case 'login':
+          x = document.getElementById('id_password');
+          break;
+  }
+
+  if(x.type === 'password'){
+      x.type = 'text';
+  }
+  else{
+      x.type = 'password'
+  }
+}
 
 
 // async function sub() {
